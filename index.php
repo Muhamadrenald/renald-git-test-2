603f097bba97bc29b146ba0d6d33a751d9def37a
Muhamad Renald <avryso@gmail.com>
2023-08-31 16:20:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut enim vero odio eaque vel consequuntur aut repudiandae aspernatur nam iste, dolorem cumque eveniet doloremque officia nihil nesciunt accusantium accusamus dolorum.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
